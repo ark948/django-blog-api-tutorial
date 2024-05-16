b041c546e9476dd925bf15a1c143041825040915
ark948 <armankarimi948@gmail.com>
2024-05-16 22:19:55 +0330

    Modified detailview page

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from posts.models import Post
 from posts.serializers import PostSerializer
 
@@ -7,5 +7,6 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAdminUser,) # only admin users can view detail page
     queryset = Post.objects.all()
     serializer_class = PostSerializer
