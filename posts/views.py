c5f1a9fcf7ed08324e09a9c6062c3dfef6689800
ark948 <armankarimi948@gmail.com>
2024-05-21 14:21:28 +0330

    Added viewsets and routers

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,16 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from posts.models import Post
-from posts.serializers import PostSerializer
+from posts.serializers import PostSerializer, UserSerializer
 from posts.permissions import IsAuthorOrReadOnly
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAdminUser
 
-class PostList(generics.ListCreateAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
